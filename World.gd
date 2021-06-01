652dee1e50c8ac7933d9b66b88205fded05aa45c
Thalslaw <connormarshall@yahoo.co.uk>
2021-06-01 13:53:22 +0100

    Leaf and grazz

--- World.gd
+++ World.gd
@@ -6,11 +6,23 @@ var borders = Rect2(1,1,20,20)
 
 onready var cliffTileMap = $Cliff_Tile
 onready var pathTileMap	= $Path_Tile
+const grazz = preload("res://Action RPG Resources/World/Grass.tscn")
 
 func _ready():
 	randomize()
 	generate_Cliffs()
 	generate_Paths()	
+	generate_Grazz()
+
+func generate_Grazz():
+	var walker = Walker.new(Vector2(10,10), borders)
+	var map = walker.walk(DISTANCE)
+	walker.queue_free()
+	for location in map:
+		if randf() >= 0.9:
+			var grass = grazz.instance()
+			add_child(grass)
+			grass.global_position = location
 	
 func generate_Cliffs():
 	var walker = Walker.new(Vector2(10,10), borders)
