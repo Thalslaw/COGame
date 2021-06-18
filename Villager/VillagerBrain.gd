23731b1f8987d59e601aa179bac052f10ea9ec23
Darkshad100 <darkshad100@hotmail.com>
2021-06-18 10:30:18 +0100

    they move!
    slowly. but they move

--- Villager/VillagerBrain.gd
+++ Villager/VillagerBrain.gd
@@ -17,7 +17,7 @@ onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $Sword/Sword
 onready var playerhurt = $Hurtbox
 onready var speech = $dotdotdot
-onready var huntZone = $HuntZone
+onready var Hunting = $HuntZone
 
 #Villagers need brains. This is not optional.
 var brain = NeuralNet.new(5)#init args: layercount
@@ -54,7 +54,7 @@ enum {
 	TALK,
 	CHASE
 }
-var state = IDLE
+var state = MOVE
 
 
 #get base agentic satisfaction
@@ -354,24 +354,25 @@ func idle_state(delta):
 	#print("finished the idle state")
 
 func move_state(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO:
+	var velocity = Vector2.ZERO
+	var food = Hunting.isTasty
+	if food != null:
+		var chase = (food.global_position - global_position).normalized()
+		velocity = velocity.move_toward(chase * MAX_SPEED, ACCELERATION * delta)
 		speech.visible = false
-		roll_vector = input_vector
-		swordHitbox.knockback_vector = input_vector
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Move/blend_position", input_vector)
-		animationTree.set("parameters/Attack/blend_position", input_vector)
-		animationTree.set("parameters/Roll/blend_position", input_vector)
+		roll_vector = velocity
+		#swordHitbox.knockback_vector =
+		animationTree.set("parameters/Idle/blend_position", velocity)
+		animationTree.set("parameters/Move/blend_position", velocity)
+		animationTree.set("parameters/Attack/blend_position", velocity)
+		animationTree.set("parameters/Roll/blend_position", velocity)
 		animationState.travel("Move")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		hungry()
+	
+	velocity = move_and_slide(velocity)
 	
 	move()
 	
@@ -403,6 +404,9 @@ func talk_state(delta): #my junky talk command thingy...
 	speech.visible = true
 	talk_end()
 
+func hungry():
+	if Hunting.smell_noms():
+		state = MOVE
 
 func _physics_process(delta):
 	
