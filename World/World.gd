aa003e0057291ca8f997043f8a1ef36f0f31b94d
Thalslaw <connormarshall@yahoo.co.uk>
2021-07-24 12:02:42 +0100

    re-tweaks to fit region zoning idea.

--- World/World.gd
+++ World/World.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 const DISTANCE = 25
 
-var borders = Rect2(1,1,18,18) ## keep these within the bounds of the tile, for use with large objects
-var bigBorders = Rect2(1,1,36,36) ## keep these twice the size of borders, for use with small objects
+var smolBorders = Rect2(1,1,9,9) ## for use with objects size 64
+var borders = Rect2(1,1,18,18) ## keep these within the bounds of the tile, for use with objects size 32
+var bigBorders = Rect2(1,1,36,36) ## keep these twice the size of borders, for use with objects size 16
 
 onready var cliffTileMap = $Cliff_Tile
 onready var pathTileMap	= $Path_Tile
@@ -11,6 +12,7 @@ onready var things = $Things
 const grazz = preload("res://Action RPG Resources/World/Grass.tscn")
 
 enum {VALLEY, VILLAGE, DUNGEON, DWELLING}
+## valley is notionally the default startpoint. Arranged this way in case we want to have different kinds of start point in the future.
 var environment = VALLEY
 
 func saveEnvironment():
@@ -26,10 +28,16 @@ func loadEnvironment():
 func _ready():
 	randomize()
 	if(environment == VALLEY):
-		generate_Cliffs()
-		generate_Paths()	
-		generate_Grazz()
-		generate_Exits()
+		#make a valley zone
+		var walker = Walker.new(Vector2(9,9), borders)
+		var map = walker.walk(DISTANCE)
+		walker.queue_free()
+		for location in map:
+			cliffTileMap.set_cellv(location, -1)
+			cliffTileMap.update_bitmask_region(borders.position, borders.end)
+		## TO DO in the perimeter of 'map', make some exit caves
+		generate_Paths() #this ought to be between the exit caves, so pass in the data.
+		generate_Grazz() #dressings
 	elif(environment == VILLAGE):
 		#the same as a valley but with dwellings
 		pass
@@ -39,22 +47,6 @@ func _ready():
 	elif(environment == DWELLING):
 		#the same as a dungeon but with dwelling dressing... people live here!
 		pass
-	
-func generate_Exits():
-	##the purpose of this is to give the tile some exits that can be reached by the playable area.
-	var walker = Walker.new(Vector2(9,18), borders)
-	var map = walker.walk(DISTANCE)
-	walker.position = Vector2(18,9)
-	map = walker.walk(DISTANCE)
-	walker.position = Vector2(1,9)
-	map = walker.walk(DISTANCE)
-	walker.position = Vector2(9,1)
-	map = walker.walk(DISTANCE)
-	walker.queue_free()
-	for location in map:
-		cliffTileMap.set_cellv(location, -1)
-	cliffTileMap.update_bitmask_region(borders.position, borders.end)
-	#TODO next search for some south facing cliffs to shove cave entrances on
 
 func generate_Grazz():
 	var walker = Walker.new(Vector2(36,36), bigBorders)
@@ -68,16 +60,10 @@ func generate_Grazz():
 				#find the ysort, add the grass there
 				things.add_child(grass)				
 				grass.global_position = location
-	
-func generate_Cliffs():
-	var walker = Walker.new(Vector2(9,9), borders)
-	var map = walker.walk(DISTANCE)
-	walker.queue_free()
-	for location in map:
-		cliffTileMap.set_cellv(location, -1)
-	cliffTileMap.update_bitmask_region(borders.position, borders.end)
 
 func generate_Paths():
+	#currently randomly walks, intent is to pass in locations to try to get between.
+	#they probably shouldn't be perfectly optimised.
 	var walker = Walker.new(Vector2(36,36), bigBorders)
 	var map = walker.walk(DISTANCE)
 	walker.queue_free()
@@ -86,9 +72,12 @@ func generate_Paths():
 			pathTileMap.set_cellv(location, 0)
 	pathTileMap.update_bitmask_region(bigBorders.position, bigBorders.end)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#this will handle elapsing time within the game.
 	#decrement a timer by delta.
+	#everything in the current zone is loaded, therefore doesn't require processing.
+	#computationally arranged:
 	#every timer loop, take the top global location in the list, and advance it as many time steps since its last elapse.
-	#move thing from the top of the list to the bottom of the list, reset the timer.
+		#
+		#move thing from the top of the list to the bottom of the list, reset the timer.
 	pass
