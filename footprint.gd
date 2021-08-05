68e9c7ad10146073b27ef5cf52e3926681332f56
Darkshad100 <darkshad100@hotmail.com>
2021-08-05 13:34:56 +0100

    Footfetish

---
+++ footprint.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+# get yo goot fetish worked out here
+#entity moves, drops a thing, thing gets it's own position
+#then thing talks to other things on spawn to make it work somehow
+#then building things signal hurtboxes on collision for some reason? idk
